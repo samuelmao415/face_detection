e13f9e0f4d46710712037652981bab7c1d046720
samuelmao415 <samuelmao415@gmail.com>
2018-12-10 17:03:14 -0500

    changed output image size

--- FaceDetect-master/face_detect.py
+++ FaceDetect-master/face_detect.py
@@ -1,8 +1,8 @@
 import cv2
 
-
 # Get user supplied values
-imagePath = ['beatles.png', "abba.png"]
+imagePath = ['beatles.png', "abba.png",
+"instagram.png", "finery.png"]
 cascPath = "haarcascade_frontalface_default.xml"
 scaleFactor = 1.1
 minNeighbors=5
@@ -39,8 +39,11 @@ def face_dection(imagePath, cascPath, scaleFactor, minNeighbors, minSize):
         detection_result[i]=len(faces)
         print("Found {0} faces!".format(len(faces)))
 
-        cv2.imshow("Faces found", image)
+        cv2.namedWindow("output", cv2.WINDOW_NORMAL)        # Create window with freedom of dimensions
+        imS = cv2.resize(image, (1280, 720))                    # Resize image
+        cv2.imshow("output", imS)                            # Show image
         cv2.waitKey(0)
+
     print(detection_result)
 
 face_dection(imagePath, cascPath, scaleFactor, minNeighbors, minSize)
